b765ea0181fbb17b1475384e6e331acadf0f39a9
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-02-26 13:32:19 +0100

    Add information about merging branches. Move a bit.

--- pkg_devel.R
+++ pkg_devel.R
@@ -127,11 +127,9 @@ devtools::install(quick = FALSE, upgrade = "never", build_vignettes = TRUE)
 # needs to be followed by install(..., build_vignettes = TRUE).
 
 
-#### Preparing for updates
-# Restyle and publish the NEWS-file at each new package release.
-devtools::document()
-
+#### Check tests ####
 # Check for which functions no test file has been written.
+devtools::document()
 files_R <- list.files(file.path(".", "R"))
 files_man <- list.files(path = file.path(".", "man"), pattern = "\\.Rd$")
 expected_man <- sub(pattern = "\\.Rd$", replacement = ".R", x = files_man)
@@ -139,9 +137,6 @@ expected_files <- sort(unique(paste0("test_", c(files_R, expected_man))))
 expected_files[!(expected_files %in% list.files("./inst/tinytest"))]
 
 tinytest::test_all()
-
-# Using 'manual = FALSE' because building the manual leads to latex errors.
-devtools::check(manual = FALSE, force_suggests = TRUE, run_dont_test = TRUE)
 
 
 #### Updating dependencies ####
@@ -175,22 +170,55 @@ usethis::use_import_from(package = "BiocManager", fun = "valid")
 # no longer ... .
 
 
-#### Test package ####
+#### Preparing for updates ####
+# Do:
+# - Restyle and publish the NEWS-file at each new package release.
+devtools::document()
+
+# Using 'manual = FALSE' because building the manual leads to latex errors.
+devtools::check(manual = FALSE, force_suggests = TRUE, run_dont_test = TRUE)
+
 .libPaths() # Check if output of .libPaths() is correct.
 devtools::install(quick = FALSE, upgrade = "never", build_vignettes = TRUE)
 
 # Load the package and view the help files as usual outside devtools:
-library(checkinput)
-?all_names
+library(basename(getwd()), character.only = TRUE)
+?reorder_levels
 browseVignettes(package = basename(getwd()))
 # If no vignettes are visible, devtools::install() was probably run with the
 # default 'build_vignettes = FALSE': change it to use 'build_vignettes = TRUE'.
 
 
-#### Setting up new GitHub branch ####
-# Then merge the devel-branch with master.
-# After a successful merge, you can delete the devel-branch.
+#### Merging devel-branch with master ####
+# To merge the devel-branch with master, go the the devel-branch on GitHub.
+# <Contribute> > Open Pull Request.
+# Copy the updated 'NEWS' in the 'description' field and use the button 'Create
+# pull request'.
 
+# If you see 'No conflicts with base branch: Merging can be performed
+# automatically', you can use the green 'Merge pull request' button, or change to
+# 'Squash and merge'.
+
+# Otherwise, you will see 'This branch has conflicts that must be resolved':
+# follow the instructions to resolve the conflicts. Then push the button 'Commit
+# merge'. Then you will see 'No conflicts with base branch' and you can proceed
+# as described in the previous paragraph.
+
+# After a successful merge, you will see a message that you can delete the
+# devel-branch, which you can do. To do so later, go to 'Pull requests', select
+# the 'closed', and scroll down to the button 'Delete branch'.
+
+# In RStudio, open the relevant project to check there are no commits left. Then
+# you can move to the 'master' branch and pull to get all updates you just
+# committed to the master branch, or first delete the project and pull the fresh
+# one.
+
+# Then you can delete the old package versions (see .libPaths()) and install the
+# updated versions following your own instructions on the GitHub pages of the
+# relevant packages.
+
+
+#### Setting up new GitHub branch ####
 # Then set up a new GitHub branch:
 # - At the GitHub page of the package:
 #   'Branch' icon > green `New branch` button > `<pkgname>_devel > green `Create new branch` button.
