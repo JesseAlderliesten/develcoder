dc22d0bcf4f8563d0fa5bb348de93a628450675b
JesseAlderliesten <59017087+JesseAlderliesten@users.noreply.github.com>
2026-04-28 14:43:11 +0200

    Move from pkg_devel.R to pkg_devel.Rmd (in progress).

--- pkg_devel.R
+++ pkg_devel.R
@@ -1,92 +1,3 @@
-#### Install required packages ####
-required_pkgs <- c("devtools", "tinytest", "usethis")
-bool_install <- !vapply(X = required_pkgs, FUN = requireNamespace,
-                        FUN.VALUE = logical(1), quietly = TRUE)
-pkgs_install <- required_pkgs[bool_install]
-if(length(pkgs_install) > 0L) {
-  utils::install.packages(pkgs = pkgs_install, lib = NULL, dependencies = NA,
-                          quiet = FALSE)
-}
-
-
-#### Add functions ####
-##### Setting up #####
-# Write function and documentation
-usethis::use_r("is_number")
-devtools::document() # also runs devtools::load_all()
-?is_number() # view help-page of the function
-# Do:
-# - Manually update the NEWS-file.
-
-
-##### Components of documentation #####
-#' Title
-#'
-#' Description
-#'
-#' @inheritParams is_logical # From another function in the current package
-#' @inheritParams utils::installed.packages # from a function in another package
-#' @param x Vector of names to test.
-#' @param allow_underscores `TRUE` or `FALSE`: allow underscores?
-#' @param x,y Separate arguments by a comma without a space to create a single
-#' description for multiple arguments.
-#'
-#' @details
-#'
-#' @returns
-#' `TRUE` or `FALSE`, returned [invisibly][invisible].
-#'
-#' @section Side effects:
-#' Values in `x` are identified in an [error][stop], [warning], or [message] if
-#' `signal` is `error`, `warn`, or `message`, respectively. Set `signal` to
-#' `quiet` to suppress this.
-#'
-#' @section Notes:
-#' Some notes.
-#'
-#' @section Programming notes:
-#' Some programming notes. Create a GitHub issue instead of To do / Wishlist
-#' sections.
-#'
-#' @seealso
-#' Section `Details` of [make.names()] and the [\R FAQ about valid names](
-#' https://CRAN.R-project.org/doc/manuals/R-FAQ.html#What-are-valid-names_003f)
-#' on the syntactical validity of names.
-#'
-#' @family collections of checks on type and length
-#'
-#' @examples
-#' all_names(x = c("a", "b1a")) # TRUE
-#'
-#' @export
-func_name <- function(x, allow_underscores = TRUE) {
-  stopifnot(is_logical(allow_underscores))
-}
-
-##### To inherit sections #####
-# Inherited sections are *silently* ignored if a section is also defined.
-
-#' @inherit outcome return
-#' @inherit is_number details
-#' @inheritSection is_logical Programming notes
-#' @inheritSection is_logical @note
-
-
-##### Adding README #####
-# To provide a nice overview of functions: see https://github.com/MJobinASU/MontyHall
-
-#### Add tests ####
-library(tinytest)
-tinytest::test_all() # Run all tests of a package
-tinytest::run_test_file("./inst/tinytest/test_funcname.R") # Run specific file
-# Note: expect_silent(expect_equal()) does NOT test that no messages are emitted!
-expect_silent(expect_true(func_name(x = x, arg = arg)))
-expect_warning(
-  expect_equal(func_name(x = "a", arg = arg), 3),
-  pattern = "...", strict = TRUE, fixed = TRUE)
-expect_error(func_name(x = "a", arg = arg),
-             pattern = "is_number(x) is not TRUE", fixed = TRUE)
-
 ##### Using temporary files #####
 # - Could also use local(...)? See withr::with_tempfile()
 #   \dontshow{
@@ -96,52 +7,6 @@ expect_error(func_name(x = "a", arg = arg),
 #   See also https://stat.ethz.ch/pipermail/r-devel/2018-March/075783.html
 # - On the problem of changing relative paths depending on how a check is run,
 #   see `testthat::test_path()`.
-
-
-#### Add vignettes ####
-##### Setting up #####
-usethis::use_vignette("my_vignette", title = "Some title")
-usethis::use_package(package = "knitr", type = "Suggests")
-usethis::use_package(package = "rmarkdown", type = "Suggests")
-devtools::document()
-devtools::build_vignettes()
-browseVignettes(package = basename(getwd()))
-
-# If no vignettes are visible, devtools::install() was probably run with the
-# default 'build_vignettes = FALSE': change it to use 'build_vignettes = TRUE:
-devtools::install(quick = FALSE, upgrade = "never", build_vignettes = TRUE)
-
-# See also: tools::pkgVignettes(package = "checkrpkgs") and
-# utils::vignette(<"vignette_title">)
-# Rendering Rd-file as plain-text help in the R console:
-# tools::Rd2txt("path/to/file.Rd")
-
-
-##### Styling #####
-# Put the next lines in the header of vignettes to get a table of contents:
-# output:
-#   rmarkdown::html_vignette:
-#     toc: true
-#     toc_depth: 3
-
-# See also my Chrome Bookmarks R > Workflow > RMarkdown.
-
-##### Linking #####
-# For internal links to other sections in the same document ('Section', 'above',
-# 'below'): [<Section title>] or [<link text>][<Section title>]
-
-# To link from vignettes to help-pages in the same package, use
-# [someFun](../help/someFun) (from https://stackoverflow.com/questions/34946219).
-# Text between angled brackets (e.g., <pkgname>) disappears when used in link
-# text, so instead of [library(<pkgname>)](../help/library), use
-# [library](../help/library)`(<pkgname>)`.
-# For a more advanced way of linking from vignettes to help-pages see
-# https://github.com/dmurdoch/rgl/commit/bbc84447c2a6efed42907fbac176e9569b868d8f
-
-# To link from help-pages to vignettes, use:
-# The [vignette about <some description>](../doc/<filename>.html).
-# If no vignettes are visible, run devtools::build_vignettes(), which possibly
-# needs to be followed by install(..., build_vignettes = TRUE).
 
 
 #### Check tests ####
@@ -154,36 +19,6 @@ expected_files <- sort(unique(paste0("test_", c(files_R, expected_man))))
 expected_files[!(expected_files %in% list.files("./inst/tinytest"))]
 
 tinytest::test_all()
-
-
-#### Updating dependencies ####
-# To declare a minimum version for R itself:
-usethis::use_package("R", type = "Depends", min_version = "4.0.0")
-# It is useful to specify the minimum declared R version as workflow in GitHub
-# Actions, see the section 'Use GitHub Actions'.
-
-# To use a function that is present in 'new' versions of R also in older versions
-# of R, see https://github.com/yihui/xfun/blob/main/R/zzz.R
-
-# 'use_package()' to declare minimum version and 'use_dev_package()' to set remote.
-usethis::use_package(package = "checkinput", type = "Imports", min_version = TRUE)
-usethis::use_dev_package(package = "checkinput", type = "Imports",
-                         remote = "github::JesseAlderliesten/checkinput")
-
-##### Importing packages or functions #####
-# There are two ways to use functions from packages that are imported (i.e., are
-# in the 'Imports:' field of the DESCRIPTION file): (1) use the package name
-# followed by two colons and the function name, e.g., utils::osVersion(...);
-# (2) In addition to the 'Imports:' field of the DESCRIPTION file, list the
-# function in the NAMESPACE file, e.g., importFrom(utils, osVersion). With
-# 'usethis', this can be achieved by  the following line:
-# usethis::use_import_from(package = "utils", fun = "osVersion"). That adds the
-# line `#' @importFrom utils osVersion` in R/<pkgname>-package.R that was
-# created by usethis::use_package_doc(), and adds the line `Imports: utils` to
-# the DESCRIPTION file (you might have to use devtools::document() for this to
-# take effect).
-usethis::use_import_from(package = "BiocManager", fun = "valid")
-usethis::use_package("tinytest", type = "Suggests", min_version = "1.4.1")
 
 
 #### Increment package version ####
@@ -212,34 +47,6 @@ library(basename(getwd()), character.only = TRUE)
 browseVignettes(package = basename(getwd()))
 # If no vignettes are visible, devtools::install() was probably run with the
 # default 'build_vignettes = FALSE': change it to use 'build_vignettes = TRUE'.
-
-
-#### Using pkgdown ####
-# See https://r-pkgs.org/website.html and https://pkgdown.r-lib.org/
-
-# Updating website
-pkgdown::build_site()
-
-# Open docs/index.html in a webbrowser to preview the website, or look at the
-# files that constitute your package’s website are in the local docs/ directory.
-
-
-#### Use GitHub Actions ####
-# To manually run GitHub Actions (GHA) set up 'workflow_dispatch' (see section
-# 'Use GitHub Actions' in pkg_setup.R). To check if package B, which depends on
-# package A, is still working fine, go to the 'Actions' tab of repository B,
-# select the action you want to trigger (e.g., R-CMD-check.yaml), and use the
-# 'Run workflow' button to run the GHA. You can select which branch it should
-# run on, but you need to trigger it once manually on the main branch to be
-# able to trigger it manually on other branches.
-
-# Scheduled jobs that failed can be rerun through the button 'Re-run jobs' >
-# 'Re-run failed jobs' > 'Re-run jobs'.
-
-# If the package declares a dependency on a specific R version, it is useful to
-# specify the minimum declared r version to run in addition to the ones that are
-# by default used in the template: add '- {os: ubuntu-latest,   r: '4.0.0'}' to
-# the 'matrix: config:' part to run R 4.0.0.
 
 
 #### Merging devel-branch with master ####
