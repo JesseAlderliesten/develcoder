7e0fad5591a034f6a93b73940ee6a0c000dbd123
JesseAlderliesten <59017087+JesseAlderliesten@users.noreply.github.com>
2026-07-04 12:34:18 +0200

    Rename argument `testdir` to `status_testdir` and variable `testdir` to `path_testdir`.

--- inst/tinytest/test_diagnose_test_files.R
+++ inst/tinytest/test_diagnose_test_files.R
@@ -4,75 +4,75 @@
 my_tempdir <- progutils::create_tempdir(prefix = "test_get_testfiles")
 withr::local_dir(new = my_tempdir)
 pkg_name <- basename(my_tempdir)
-testdir <- fs::path(my_tempdir, "inst", "tinytest")
+path_testdir <- fs::path(my_tempdir, "inst", "tinytest")
 
 #####  testdir missing #####
 expect_identical(
-  diagnose_test_files(path = testdir),
-  list(pkg = pkg_name, path = testdir, pattern = "^test_|^test-",
-       ignore_case = TRUE, testdir = "missing", status_test_files = "missing",
+  diagnose_test_files(path = path_testdir),
+  list(pkg = pkg_name, path = path_testdir, pattern = "^test_|^test-",
+       ignore_case = TRUE, status_testdir = "missing", status_test_files = "missing",
        test_files = character(0), ignored_files = character(0))
 )
 
 ##### testdir empty #####
-fs::dir_create(path = testdir)
+fs::dir_create(path = path_testdir)
 expect_identical(
-  diagnose_test_files(path = testdir),
-  list(pkg = pkg_name, path = testdir, pattern = "^test_|^test-",
-       ignore_case = TRUE, testdir = "present", status_test_files = "missing",
+  diagnose_test_files(path = path_testdir),
+  list(pkg = pkg_name, path = path_testdir, pattern = "^test_|^test-",
+       ignore_case = TRUE, status_testdir = "present", status_test_files = "missing",
        test_files = character(0), ignored_files = character(0))
 )
 
 ##### testdir not empty but not containing testfiles #####
-path_misc_file <- fs::path(testdir, "misc_file.R")
+path_misc_file <- fs::path(path_testdir, "misc_file.R")
 fs::file_create(path_misc_file)
 expect_identical(
-  diagnose_test_files(path = testdir),
-  list(pkg = pkg_name, path = testdir, pattern = "^test_|^test-",
-       ignore_case = TRUE, testdir = "present", status_test_files = "missing",
+  diagnose_test_files(path = path_testdir),
+  list(pkg = pkg_name, path = path_testdir, pattern = "^test_|^test-",
+       ignore_case = TRUE, status_testdir = "present", status_test_files = "missing",
        test_files = character(0), ignored_files = "misc_file.R")
 )
 
 ##### 'pattern' is respected #####
 expect_identical(
-  diagnose_test_files(path = testdir, pattern = "^misc_"),
-  list(pkg = pkg_name, path = testdir, pattern = "^misc_",
-       ignore_case = TRUE, testdir = "present", status_test_files = "fine",
+  diagnose_test_files(path = path_testdir, pattern = "^misc_"),
+  list(pkg = pkg_name, path = path_testdir, pattern = "^misc_",
+       ignore_case = TRUE, status_testdir = "present", status_test_files = "fine",
        test_files = "misc_file.R", ignored_files = character(0))
 )
 
 ##### 'ignore_case' is respected #####
 expect_identical(
-  diagnose_test_files(path = testdir, pattern = "^MIsc_", ignore_case = TRUE),
-  list(pkg = pkg_name, path = testdir, pattern = "^MIsc_",
-       ignore_case = TRUE, testdir = "present", status_test_files = "fine",
+  diagnose_test_files(path = path_testdir, pattern = "^MIsc_", ignore_case = TRUE),
+  list(pkg = pkg_name, path = path_testdir, pattern = "^MIsc_",
+       ignore_case = TRUE, status_testdir = "present", status_test_files = "fine",
        test_files = "misc_file.R", ignored_files = character(0))
 )
 
 expect_identical(
-  diagnose_test_files(path = testdir, pattern = "^MIsc_", ignore_case = FALSE),
-  list(pkg = pkg_name, path = testdir, pattern = "^MIsc_",
-       ignore_case = FALSE, testdir = "present", status_test_files = "missing",
+  diagnose_test_files(path = path_testdir, pattern = "^MIsc_", ignore_case = FALSE),
+  list(pkg = pkg_name, path = path_testdir, pattern = "^MIsc_",
+       ignore_case = FALSE, status_testdir = "present", status_test_files = "missing",
        test_files = character(0), ignored_files = "misc_file.R")
 )
 
 ##### testdir containing both ignored files and testfiles #####
-fs::file_create(fs::path(testdir, "test_file.R"))
+fs::file_create(fs::path(path_testdir, "test_file.R"))
 expect_identical(
-  diagnose_test_files(path = testdir, pattern = "^test_|^test-"),
-  list(pkg = pkg_name, path = testdir, pattern = "^test_|^test-",
-       ignore_case = TRUE, testdir = "present", status_test_files = "wrong",
+  diagnose_test_files(path = path_testdir, pattern = "^test_|^test-"),
+  list(pkg = pkg_name, path = path_testdir, pattern = "^test_|^test-",
+       ignore_case = TRUE, status_testdir = "present", status_test_files = "wrong",
        test_files = "test_file.R", ignored_files = "misc_file.R")
 )
 
 ##### testdir containing only testfiles #####
-fs::file_create(fs::path(testdir, "test_file2.R"))
+fs::file_create(fs::path(path_testdir, "test_file2.R"))
 unlink(path_misc_file)
 
 expect_identical(
-  diagnose_test_files(path = testdir, pattern = "^test_|^test-"),
-  list(pkg = pkg_name, path = testdir, pattern = "^test_|^test-",
-       ignore_case = TRUE, testdir = "present", status_test_files = "fine",
+  diagnose_test_files(path = path_testdir, pattern = "^test_|^test-"),
+  list(pkg = pkg_name, path = path_testdir, pattern = "^test_|^test-",
+       ignore_case = TRUE, status_testdir = "present", status_test_files = "fine",
        test_files = c("test_file.R", "test_file2.R"), ignored_files = character(0))
 )
 
@@ -92,10 +92,10 @@ expect_warning(
   strict = TRUE, fixed = TRUE)
 
 expect_error(
-  diagnose_test_files(path = testdir, ignore_case = NA),
+  diagnose_test_files(path = path_testdir, ignore_case = NA),
   pattern = "is_logical(ignore_case) is not TRUE", fixed = TRUE)
 
 
 #### Remove objects used in tests ####
 unlink(my_tempdir, recursive = TRUE)
-rm(my_tempdir, path_misc_file, pkg_name, testdir)
+rm(my_tempdir, path_misc_file, path_testdir, pkg_name)
